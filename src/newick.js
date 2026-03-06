5c5eba80d02a4f3b51283de8fffad5beaed49781
Jason Davies <jason@jasondavies.com>
2026-03-06 11:34:40 +0000

    Fix root-only tree parsing and modernize Node tests

--- src/newick.js
+++ src/newick.js
@@ -83,7 +83,7 @@
           break;
         default:
           var x = tokens[i-1];
-          if (x == ')' || x == '(' || x == ',') {
+          if ((i === 0 && token !== '') || x == ')' || x == '(' || x == ',') {
             tree.name = token;
           } else if (x == ':') {
             tree.length = parseFloat(token);
--- test/test.js
+++ test/test.js
@@ -1,18 +1,25 @@
-/**
- * TO DO: Add more tests!
- */
-var parse = require('../src/newick').parse;
-var print = require('sys').print;
+const test = require("node:test");
+const assert = require("node:assert/strict");
+const parse = require("../src/newick").parse;
 
-var x = parse('(A: 0.1,B: 0.2,(C:0.3,D:0.4)E:0.5)F;');
-var assert = function(x) {
-  if (x) {
-    print(".");
-  } else {
-    print("F");
-  }
-};
-assert(x.name == 'F');
-assert(x.branchset.length == 3);
-assert(x.branchset[0].name == 'A');
-print('\n');
+test("parses a tree with names and branch lengths", () => {
+  const tree = parse("(A: 0.1,B: 0.2,(C:0.3,D:0.4)E:0.5)F;");
+
+  assert.equal(tree.name, "F");
+  assert.equal(tree.branchset.length, 3);
+  assert.deepEqual(tree.branchset[0], { name: "A", length: 0.1 });
+  assert.deepEqual(tree.branchset[1], { name: "B", length: 0.2 });
+  assert.deepEqual(tree.branchset[2], {
+    name: "E",
+    length: 0.5,
+    branchset: [
+      { name: "C", length: 0.3 },
+      { name: "D", length: 0.4 }
+    ]
+  });
+});
+
+test("parses a root-only tree name", () => {
+  const tree = parse("A;");
+  assert.equal(tree.name, "A");
+});
